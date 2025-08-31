33662f7a095be901c0dcd66cc30e2eb3f73b2a72
Vaibhav Jain <vaibhavjain0724@gmail.com>
2025-08-31 14:55:06 +0530

    m

--- backend/db.js
+++ backend/db.js
@@ -10,14 +10,25 @@ const Notes = new Schema({
 
 const NotesModel = mongoose.model('notes', Notes);
 
+// async function connectDB() {
+//     try {
+//         await mongoose.connect("mongodb+srv://admin:Llyu9sVM4MkupHOy@cluster0.vj9fwxg.mongodb.net/notes");
+//         console.log("Connected to MongoDB Atlas");
+//     } catch (error) {
+//         console.error("MongoDB connection error:", error.message);
+//     }
+// }
+
+
 async function connectDB() {
     try {
-        await mongoose.connect("mongodb+srv://admin:Llyu9sVM4MkupHOy@cluster0.vj9fwxg.mongodb.net/notes");
+        await mongoose.connect("mongodb+srv://vaibhavjain0724_db_user:ykXNoTNrsvXEVVLB@cluster0.vyidpbt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
         console.log("Connected to MongoDB Atlas");
     } catch (error) {
         console.error("MongoDB connection error:", error.message);
     }
 }
+
 
 module.exports= {
     NotesModel : NotesModel,
